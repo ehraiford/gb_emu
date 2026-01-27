920d8895803a4778668373ec73486083894b0804
Evan Raiford <evanraiford@gmail.com>
2026-01-27 18:28:19 -0500

    Updated memory map to better handle bootrom tradeoff and setting devices as inaccessible during PPU modes and DMA transfer

--- gb_emu/src/bus.rs
+++ gb_emu/src/bus.rs
@@ -2,9 +2,11 @@ use std::fmt::Display;
 
 use crate::{
     cartridge::cartridge::{Cartridge, CartridgeDevice},
-    game_boy::Change,
+    game_boy::GameBoyStateChange,
     graphics::{
+        lcd::LcdRegisters,
         oam::{ObjectAttributeMemory, PriorityMode},
+        ppu::PpuTickMode,
         video_ram::VideoRam,
     },
     helper_functions::{concat_2_bytes, log},
@@ -32,21 +34,28 @@ pub struct Bus {
     h_ram: HighRam,
     ie: InterruptEnableRegister,
     boot_rom: BootRom,
+    memory_map: MemoryMap,
 }
 
 impl Bus {
     pub fn load_cartridge(&mut self, cartridge: Cartridge) {
         self.cartridge = cartridge;
     }
-    pub fn unmap_bootrom(&mut self) {
-        self.boot_rom.unmap();
+
+    pub fn handle_memory_map_event(&mut self, event: MemoryMapEvent) {
+        self.memory_map.handle_memory_map_event(event)
     }
 
-    pub fn start_oam_dma_transfer(&mut self) {
-        todo!("HRAM is the only accessible part of memory")
+    fn get_cpu_accessible_device_from_address(&self, address: Address) -> Option<MemoryTarget> {
+        self.memory_map.get_cpu_accessible_device_from_address(address)
     }
-    pub fn end_oam_dma_transfer(&mut self) {
-        todo!("HRAM is no longer the only accessible part of memory")
+
+    fn get_device_from_address(&self, address: Address) -> MemoryTarget {
+        self.memory_map.get_device_from_address(address)
+    }
+
+    pub fn get_ppu_context_mem(&mut self) -> (&mut VideoRam, &mut ObjectAttributeMemory, &mut LcdRegisters) {
+        (&mut self.v_ram, &mut self.oam, &mut self.io_registers.lcd_registers)
     }
 
     pub fn read_u16(&mut self, address: Address) -> BusAccessOutcome<u16> {
@@ -91,12 +100,12 @@ impl Bus {
     }
 
     pub fn read(&mut self, address: Address) -> BusAccessOutcome<u8> {
-        let device = MemoryTarget::get_device_from_address(address);
+        let Some(device) = self.get_cpu_accessible_device_from_address(address) else {
+            return BusAccessFailure::InaccessibleByCpu.into();
+        };
         match device {
-            MemoryTarget::RomBank00 => match self.boot_rom.mapped() && address < BootRom::SIZE {
-                true => self.boot_rom.read(address),
-                false => self.cartridge.read(address, CartridgeDevice::RomBank00),
-            },
+            MemoryTarget::BootRom => self.boot_rom.read(address),
+            MemoryTarget::RomBank00 => self.cartridge.read(address, CartridgeDevice::RomBank00),
             MemoryTarget::BankableRom => self.cartridge.read(address, CartridgeDevice::BankableRom),
             MemoryTarget::VideoRam => self.v_ram.read(address),
             MemoryTarget::ExternalRam => self.cartridge.read(address, CartridgeDevice::ExternalRam),
@@ -113,8 +122,9 @@ impl Bus {
 
     /// Statelessly access the value at the given address. This is for dma transfers and observational use
     pub fn peek(&self, address: Address) -> u8 {
-        let device = MemoryTarget::get_device_from_address(address);
+        let device = self.get_device_from_address(address);
         match device {
+            MemoryTarget::BootRom => self.boot_rom.read(address).0,
             MemoryTarget::RomBank00 => self.cartridge.peek(address, CartridgeDevice::RomBank00),
             MemoryTarget::BankableRom => self.cartridge.peek(address, CartridgeDevice::BankableRom),
             MemoryTarget::VideoRam => self.v_ram.peek(address),
@@ -130,8 +140,14 @@ impl Bus {
         }
     }
     pub fn write(&mut self, address: Address, value: u8) -> BusAccessOutcome<()> {
-        let device = MemoryTarget::get_device_from_address(address);
+        let Some(device) = self.get_cpu_accessible_device_from_address(address) else {
+            return BusAccessFailure::InaccessibleByCpu.into();
+        };
+        // if device == MemoryTarget::VideoRam {
+        //     println!("VRAM Address: {address:04x}");
+        // }
         match device {
+            MemoryTarget::BootRom => self.boot_rom.write(address, value),
             MemoryTarget::RomBank00 => self.cartridge.write(address, CartridgeDevice::RomBank00, value),
             MemoryTarget::BankableRom => self.cartridge.write(address, CartridgeDevice::BankableRom, value),
             MemoryTarget::VideoRam => self.v_ram.write(address, value),
@@ -168,6 +184,7 @@ impl Bus {
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 pub enum MemoryTarget {
+    BootRom,
     RomBank00,
     BankableRom,
     VideoRam,
@@ -185,6 +202,7 @@ pub enum MemoryTarget {
 impl MemoryTarget {
     pub const fn get_base_address(&self) -> Address {
         match self {
+            MemoryTarget::BootRom => 0x0000,
             MemoryTarget::RomBank00 => 0x0000,
             MemoryTarget::BankableRom => 0x4000,
             MemoryTarget::VideoRam => 0x8000,
@@ -199,46 +217,22 @@ impl MemoryTarget {
             MemoryTarget::InterruptEnableRegister => 0xFFFF,
         }
     }
-    pub const fn get_end_address(&self) -> Address {
+    pub const fn get_end_address_inclusive(&self) -> Address {
         match self {
-            MemoryTarget::RomBank00 => 0x4000,
-            MemoryTarget::BankableRom => 0x8000,
-            MemoryTarget::VideoRam => 0xA000,
-            MemoryTarget::ExternalRam => 0xC000,
-            MemoryTarget::WorkRam00 => 0xD000,
-            MemoryTarget::BankableWorkRam => 0xE000,
-            MemoryTarget::EchoRam => 0xFE00,
-            MemoryTarget::ObjectAttributeMemory => 0xFEA0,
-            MemoryTarget::Unusable => 0xFF00,
-            MemoryTarget::IoRegisters => 0xFF80,
-            MemoryTarget::HighRam => 0xFFFF,
+            MemoryTarget::BootRom => 0x0FF,
+            MemoryTarget::RomBank00 => 0x3FFF,
+            MemoryTarget::BankableRom => 0x7FFF,
+            MemoryTarget::VideoRam => 0x9FFF,
+            MemoryTarget::ExternalRam => 0xBFFF,
+            MemoryTarget::WorkRam00 => 0xCFFF,
+            MemoryTarget::BankableWorkRam => 0xDFFF,
+            MemoryTarget::EchoRam => 0xFDFF,
+            MemoryTarget::ObjectAttributeMemory => 0xFE9F,
+            MemoryTarget::Unusable => 0xFEFF,
+            MemoryTarget::IoRegisters => 0xFF7F,
+            MemoryTarget::HighRam => 0xFFFE,
             MemoryTarget::InterruptEnableRegister => 0xFFFF,
         }
-    }
-    pub const fn get_device_from_address(address: Address) -> Self {
-        let enumerated_devices: &[MemoryTarget] = &[
-            MemoryTarget::RomBank00,
-            MemoryTarget::BankableRom,
-            MemoryTarget::VideoRam,
-            MemoryTarget::ExternalRam,
-            MemoryTarget::WorkRam00,
-            MemoryTarget::BankableWorkRam, // GameBoy Color Only
-            MemoryTarget::EchoRam,         // Mirror of C000-DDFF
-            MemoryTarget::ObjectAttributeMemory,
-            MemoryTarget::Unusable,
-            MemoryTarget::IoRegisters,
-            MemoryTarget::HighRam,
-        ];
-
-        let mut i = 0;
-        while i < enumerated_devices.len() {
-            if enumerated_devices[i].get_end_address() > address {
-                return enumerated_devices[i];
-            }
-            i += 1;
-        }
-        // only one that doesn't fit in there is IE Register
-        MemoryTarget::InterruptEnableRegister
     }
 }
 
@@ -253,7 +247,7 @@ pub trait BusAccessible {
         Self::MM_DEVICE.get_base_address()
     }
     fn end_address(&self) -> Address {
-        Self::MM_DEVICE.get_end_address()
+        Self::MM_DEVICE.get_end_address_inclusive()
     }
 
     fn read(&mut self, address: Address) -> BusAccessOutcome<u8>;
@@ -261,9 +255,169 @@ pub trait BusAccessible {
     fn peek(&self, address: Address) -> u8;
 }
 
+struct MemoryMap {
+    map: [MemoryMapEntry; 13],
+}
+
+impl MemoryMap {
+    fn handle_memory_map_event(&mut self, event: MemoryMapEvent) {
+        match event {
+            MemoryMapEvent::UnmapBootRom => {
+                self.map[0].device = MemoryTarget::RomBank00;
+            },
+            MemoryMapEvent::UpdatePpuMode(ppu_tick_mode) => {
+                let inaccessible = ppu_tick_mode.get_cpu_inaccessible_video_targets();
+                let accessible = ppu_tick_mode.get_cpu_accessible_video_targets();
+                self.set_devices_ppu_inaccessible(&inaccessible, true);
+                self.set_devices_ppu_inaccessible(&accessible, false);
+            },
+            MemoryMapEvent::StartOamDataTransfer => self.set_devices_dma_inaccessible(
+                &[
+                    MemoryTarget::BootRom,
+                    MemoryTarget::RomBank00,
+                    MemoryTarget::BankableRom,
+                    MemoryTarget::VideoRam,
+                    MemoryTarget::ExternalRam,
+                    MemoryTarget::WorkRam00,
+                    MemoryTarget::BankableWorkRam,
+                    MemoryTarget::EchoRam,
+                    MemoryTarget::ObjectAttributeMemory,
+                    MemoryTarget::Unusable,
+                    MemoryTarget::IoRegisters,
+                ],
+                true,
+            ),
+            MemoryMapEvent::EndOamDataTransfer => self.set_devices_dma_inaccessible(
+                &[
+                    MemoryTarget::BootRom,
+                    MemoryTarget::RomBank00,
+                    MemoryTarget::BankableRom,
+                    MemoryTarget::VideoRam,
+                    MemoryTarget::ExternalRam,
+                    MemoryTarget::WorkRam00,
+                    MemoryTarget::BankableWorkRam,
+                    MemoryTarget::EchoRam,
+                    MemoryTarget::ObjectAttributeMemory,
+                    MemoryTarget::Unusable,
+                    MemoryTarget::IoRegisters,
+                ],
+                false,
+            ),
+        }
+    }
+
+    fn get_mapping_from_address(&self, address: Address) -> &MemoryMapEntry {
+        let index = match address {
+            0x0000..=0x00FF => 0,
+            0x0100..=0x3FFF => 1,
+            0x4000..=0x7FFF => 2,
+            0x8000..=0x9FFF => 3,
+            0xA000..=0xBFFF => 4,
+            0xC000..=0xCFFF => 5,
+            0xD000..=0xDFFF => 6,
+            0xE000..=0xFDFF => 7,
+            0xFE00..=0xFE9F => 8,
+            0xFEA0..=0xFEFF => 9,
+            0xFF00..=0xFF7F => 10,
+            0xFF80..=0xFFFE => 11,
+            0xFFFF => 12,
+        };
+        &self.map[index]
+    }
+
+    fn get_device_from_address(&self, address: Address) -> MemoryTarget {
+        self.get_mapping_from_address(address).device
+    }
+
+    fn get_cpu_accessible_device_from_address(&self, address: Address) -> Option<MemoryTarget> {
+        let device = self.get_mapping_from_address(address);
+        if device.inaccessible() {
+            return None;
+        } else {
+            return Some(device.device);
+        }
+    }
+
+    fn get_device_index(device: MemoryTarget) -> usize {
+        match device {
+            MemoryTarget::BootRom => 0,
+            MemoryTarget::RomBank00 => 1,
+            MemoryTarget::BankableRom => 2,
+            MemoryTarget::VideoRam => 3,
+            MemoryTarget::ExternalRam => 4,
+            MemoryTarget::WorkRam00 => 5,
+            MemoryTarget::BankableWorkRam => 6,
+            MemoryTarget::EchoRam => 7,
+            MemoryTarget::ObjectAttributeMemory => 8,
+            MemoryTarget::Unusable => 9,
+            MemoryTarget::IoRegisters => 10,
+            MemoryTarget::HighRam => 11,
+            MemoryTarget::InterruptEnableRegister => 12,
+        }
+    }
+
+    fn get_mapping_mut(&mut self, device: MemoryTarget) -> &mut MemoryMapEntry {
+        &mut self.map[Self::get_device_index(device)]
+    }
+    fn set_devices_ppu_inaccessible(&mut self, devices: &[MemoryTarget], inaccessible: bool) {
+        for device in devices {
+            self.set_device_ppu_inaccessible(*device, inaccessible);
+        }
+    }
+    fn set_devices_dma_inaccessible(&mut self, devices: &[MemoryTarget], inaccessible: bool) {
+        for device in devices {
+            self.set_device_dma_inaccessible(*device, inaccessible);
+        }
+    }
+    fn set_device_ppu_inaccessible(&mut self, device: MemoryTarget, inaccessible: bool) {
+        self.get_mapping_mut(device).inaccessible_due_to_ppu = inaccessible
+    }
+    fn set_device_dma_inaccessible(&mut self, device: MemoryTarget, inaccessible: bool) {
+        self.get_mapping_mut(device).inaccessible_due_to_dma = inaccessible
+    }
+}
+impl Default for MemoryMap {
+    fn default() -> Self {
+        Self {
+            map: [
+                MemoryMapEntry::new(MemoryTarget::BootRom),
+                MemoryMapEntry::new(MemoryTarget::RomBank00),
+                MemoryMapEntry::new(MemoryTarget::BankableRom),
+                MemoryMapEntry::new(MemoryTarget::VideoRam),
+                MemoryMapEntry::new(MemoryTarget::ExternalRam),
+                MemoryMapEntry::new(MemoryTarget::WorkRam00),
+                MemoryMapEntry::new(MemoryTarget::BankableWorkRam),
+                MemoryMapEntry::new(MemoryTarget::EchoRam),
+                MemoryMapEntry::new(MemoryTarget::ObjectAttributeMemory),
+                MemoryMapEntry::new(MemoryTarget::Unusable),
+                MemoryMapEntry::new(MemoryTarget::IoRegisters),
+                MemoryMapEntry::new(MemoryTarget::HighRam),
+                MemoryMapEntry::new(MemoryTarget::InterruptEnableRegister),
+            ],
+        }
+    }
+}
+struct MemoryMapEntry {
+    device: MemoryTarget,
+    inaccessible_due_to_ppu: bool,
+    inaccessible_due_to_dma: bool,
+}
+impl MemoryMapEntry {
+    fn new(target: MemoryTarget) -> Self {
+        Self {
+            device: target,
+            inaccessible_due_to_ppu: false,
+            inaccessible_due_to_dma: false,
+        }
+    }
+    fn inaccessible(&self) -> bool {
+        self.inaccessible_due_to_dma | self.inaccessible_due_to_ppu
+    }
+}
+
 pub type Address = u16;
 
-pub struct BusAccessOutcome<T>(pub T, pub Vec<Change>);
+pub struct BusAccessOutcome<T>(pub T, pub Vec<GameBoyStateChange>);
 
 impl<T: BusDefault> BusAccessOutcome<T> {
     pub fn default_outcome() -> Self {
@@ -272,9 +426,9 @@ impl<T: BusDefault> BusAccessOutcome<T> {
 }
 pub enum BusAccessFailure {
     NothingMappedToAddress,
-    InaccessbileInPpuMode,
+    InaccessibleByCpu,
     TriedAccessingUnusableMemory,
-    TriedWritingToRom,
+    TriedWritingToReadOnlyMemory,
     Unimplemented,
 }
 
@@ -282,18 +436,34 @@ impl Display for BusAccessFailure {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let str = match self {
             BusAccessFailure::NothingMappedToAddress => "NothingMappedToAddress",
-            BusAccessFailure::InaccessbileInPpuMode => "InaccessbileInPpuMode",
+            BusAccessFailure::InaccessibleByCpu => "InaccessibleByCpu",
             BusAccessFailure::TriedAccessingUnusableMemory => "TriedAccessingUnusableMemory",
-            BusAccessFailure::TriedWritingToRom => "TriedWritingToRom",
+            BusAccessFailure::TriedWritingToReadOnlyMemory => "TriedWritingToRom",
             BusAccessFailure::Unimplemented => "NotImplemented",
         };
         f.write_str(str)
     }
 }
 
+#[derive(Debug)]
+pub enum MemoryMapEvent {
+    UnmapBootRom,
+    UpdatePpuMode(PpuTickMode),
+    StartOamDataTransfer,
+    EndOamDataTransfer,
+}
+
+enum CpuAccessibilityTarget {
+    Cartridge,
+    VideoRam,
+    WorkRam,
+    ObjectAttributeMemory,
+    IoRegisters,
+}
+
 /// The return value for reads to inaccessible devices.
 /// Just standardizing our garbage and removing mystical numbers.
-pub const INACCESIBLE_RETURN_VALUE: u8 = 0xFF;
+pub const INACCESSIBLE_RETURN_VALUE: u8 = 0xFF;
 
 impl From<u8> for BusAccessOutcome<u8> {
     fn from(value: u8) -> Self {
@@ -315,14 +485,14 @@ impl From<()> for BusAccessOutcome<()> {
 
 impl<T: BusDefault> From<BusAccessFailure> for BusAccessOutcome<T> {
     fn from(failure: BusAccessFailure) -> Self {
-        // log(&format!("{}", failure));
+        // log(format_args!("{}", access_failure));
         Self(T::DEFAULT_BUS_VALUE, vec![])
     }
 }
 
 impl From<BusAccessFailure> for u8 {
-    fn from(access_failure: BusAccessFailure) -> Self {
-        // log(&format!("{}", access_failure));
+    fn from(failure: BusAccessFailure) -> Self {
+        // log(format_args!("{}", access_failure));
         u8::DEFAULT_BUS_VALUE
     }
 }
@@ -332,7 +502,7 @@ pub trait BusDefault {
 }
 
 impl BusDefault for u8 {
-    const DEFAULT_BUS_VALUE: Self = INACCESIBLE_RETURN_VALUE;
+    const DEFAULT_BUS_VALUE: Self = INACCESSIBLE_RETURN_VALUE;
 }
 impl BusDefault for u16 {
     const DEFAULT_BUS_VALUE: Self = 0xFFFF;
